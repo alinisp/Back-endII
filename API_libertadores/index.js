e09216999c062119c4648f8f57e3af19b052a741
alinisp <alinisperr@gmail.com>
2025-01-30 11:40:55 -0400

    enter

---
+++ API_libertadores/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+// import pool from './servico/conexao.js';
+import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
+
+
+const app = express();
+
+app.get('/campeonatos', async (req, res) => {
+    const campeonatos = await retornaCampeonatos();
+    res.json(campeonatos);
+})
+
+app.listen(9000, () => {
+    const data = new Date();
+    console.log("Servidor node iniciado em: "+data);
+
+    // const conexao = await pool.getConnection();
+
+    // console.log(conexao.threadId);
+
+    // conexao.release();
+})
+
+app.get('/campeonatos/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    const campeonato = await retornaCampeonatosID(id);
+    res.json(campeonato);
+}) 
