6ea9e292d450c6bb9fa743a0133b11c45821d87e
alinisp <alinisperr@gmail.com>
2025-02-06 11:08:22 -0400

    enter

--- API_libertadores/index.js
+++ API_libertadores/index.js
@@ -1,17 +1,22 @@
 import express from 'express';
 // import pool from './servico/conexao.js';
-import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
-import { retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
-import { retornaCampeonatosAno } from './servico/retornaCampeonatos_servico.js';
-import { retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatos, retornaCampeonatosID, retornaCampeonatosAno, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
+// import { retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
+// import { retornaCampeonatosAno } from './servico/retornaCampeonatos_servico.js';
+// import { retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
+import { cadastraCampeonatos} from './servico/cadastroCampeonatoServico.js';
 
 
 const app = express();
+app.use(express.json()); //Suporte 
 
-// app.get('/campeonatos', async (req, res) => {
-//     const campeonatos = await retornaCampeonatos();
-//     res.json(campeonatos);
-// })
+app.post('/campeonatos', async (req, res) => {
+    const campeao = req.body.campeao;
+    const vice = req.body.vice;
+    const ano = req.body.ano;
+    await cadastraCampeonatos(campeao, vice, ano);
+    res.status(204).send({"Mensagem": "Cadastro efetivado com sucesso!"});
+})
 
 app.get('/campeonatos', async (req, res) => {
     let campeonatos;
